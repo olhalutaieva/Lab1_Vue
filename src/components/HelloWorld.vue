38d25aa56991ee910bfcdf5142f90895f7736fef
Olha <57017694+olhalutaieva@users.noreply.github.com>
2023-11-09 16:12:57 +0100

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+
+
+  <div class="card">
+    <p>
+      My Name is Olia Lutaieva
+    </p>
+  </div>
+  <div class="card">
+    <p>
+      I am getting master degree
+    </p>
+  </div>
+  <div class="card">
+    <p>
+      At 121 specialistaiton
+    </p>
+  </div>
+  <div class="card">
+    <p>
+      My group 8.1212
+    </p>
+  </div>
+</template>
+
